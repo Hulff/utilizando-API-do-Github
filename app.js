28e6ec2b1469671e559dcd2d00e7bc31300a5711
Hulff <ph2717693@gmail.com>
2021-10-28 14:41:59 -0200

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+const divLogin = document.querySelector('.div_login')
+const divCracha = document.querySelector('.div_cracha')
+
+
+// usando a API
+
+function identificarUser () {
+  
+const usuario = user_login.value
+
+const url = `https://api.github.com/users/${usuario}`
+
+  fetch(url)
+.then(resposta => resposta.json())
+.then(data => {
+  if (data.message == "Not Found") {
+    console.error("Usuario Invalido ou Usuario em Branco")
+  } else {configurarUsuario()}
+})
+}
+
+function configurarUsuario() {
+
+const usuario = user_login.value
+
+const url = `https://api.github.com/users/${usuario}`
+const urlGithub = `https://github.com/${usuario}`
+
+linkGithub.href = urlGithub
+
+fetch(url)
+.then(resposta => resposta.json())
+.then(data => {
+  username.textContent = data.name
+  gitUser.textContent = data.login 
+  imgUser.src = data.avatar_url
+  bioGithub.textContent = data.bio
+})
+
+console.log("usuario preenchido")
+
+divLogin.style.display = 'none'
+divCracha.style.display = 'initial'
+
+}
